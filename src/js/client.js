026b34a944b04053ce00df54df7db70dc70b053f
Tonni <hby3387@gmail.com>
2017-04-16 17:30:27 +0800

    Fix onTouchTap issue.

--- src/js/client.js
+++ src/js/client.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
+// Material-ui needs onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
 import Layout from "./components/Layout";
 
 const __app = document.getElementById('app');
